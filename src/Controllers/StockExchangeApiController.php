be521d5fef0c4a8357d1c7591a2f36a3033aaf52
Warchiefs <ialexmur@gmail.com>
2017-08-27 22:21:32 +0300

    Controller update

--- src/Controllers/StockExchangeApiController.php
+++ src/Controllers/StockExchangeApiController.php
@@ -17,21 +17,35 @@ class StockExchangeApiController extends Controller
 
 	public function getAvailableQuotation()
 	{
-		//
+		return $this->exchange->getAvailableQuotation();
 	}
 
 	public function getInfoAboutPair(Request $request)
 	{
-		//
+		return $this->exchange->getInfoAboutPair($request->get('pair'));
 	}
 
 	public function getJsonByPair(Request $request)
 	{
-		//
+		switch (config('exchange.selected')) {
+			case 'kraken':
+				return $this->exchange->getJsonByPair($request->get('pair'), $request->get('interval'), $request->get('since'));
+			case 'poloniex':
+				return $this->exchange->getJsonByPair($request->get('pair'), $request->get('start'), $request->get('end'), $request->get('period'));
+			default:
+				return $this->exchange->getJsonByPair($request->get('pair'), $request->get('interval'));
+		}
 	}
 
 	public function getTradeHistory(Request $request)
 	{
-		//
+		switch (config('exchange.selected')) {
+			case 'kraken':
+				return $this->exchange->getTradeHistory($request->get('pair'), $request->get('since'));
+			case 'poloniex':
+				return $this->exchange->getTradeHistory($request->get('pair'), $request->get('start'), $request->get('end'));
+			default:
+				return $this->exchange->getTradeHistory($request->get('pair'), $request->get('interval'));
+		}
 	}
 }
