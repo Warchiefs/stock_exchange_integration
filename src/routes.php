3d1a008848efcfc14f0821aa0688782ade14c438
Warchiefs <ialexmur@gmail.com>
2017-08-28 00:17:57 +0300

    update config access

--- src/routes.php
+++ src/routes.php
@@ -8,26 +8,26 @@ Route::prefix('exchange/api')->group(function () {
 		return $exchange->getAvailableQuotation();
 	});
 	Route::any('/ticker', function(Exchange $exchange){
-		return $exchange->getInfoAboutPair(\Illuminate\Support\Facades\Request::get('pair'));
+		return $exchange->getInfoAboutPair(\Illuminate\Support\Facades\Request::get('first_currency'), \Illuminate\Support\Facades\Request::get('second_currency'));
 	});
 	Route::any('/chart', function(Exchange $exchange){
 		switch (Config::get('exchange.selected')) {
 			case 'kraken':
-				return $exchange->getJsonByPair(\Illuminate\Support\Facades\Request::get('pair'), \Illuminate\Support\Facades\Request::get('interval'), \Illuminate\Support\Facades\Request::get('since'));
+				return $exchange->getJsonByPair(\Illuminate\Support\Facades\Request::get('first_currency'), \Illuminate\Support\Facades\Request::get('second_currency'), \Illuminate\Support\Facades\Request::get('interval'), \Illuminate\Support\Facades\Request::get('since'));
 			case 'poloniex':
-				return $exchange->getJsonByPair(\Illuminate\Support\Facades\Request::get('pair'), \Illuminate\Support\Facades\Request::get('start'), \Illuminate\Support\Facades\Request::get('end'), \Illuminate\Support\Facades\Request::get('period'));
+				return $exchange->getJsonByPair(\Illuminate\Support\Facades\Request::get('first_currency'), \Illuminate\Support\Facades\Request::get('second_currency'), \Illuminate\Support\Facades\Request::get('start'), \Illuminate\Support\Facades\Request::get('end'), \Illuminate\Support\Facades\Request::get('period'));
 			default:
-				return $exchange->getJsonByPair(\Illuminate\Support\Facades\Request::get('pair'), \Illuminate\Support\Facades\Request::get('interval'));
+				return $exchange->getJsonByPair(\Illuminate\Support\Facades\Request::get('first_currency'), \Illuminate\Support\Facades\Request::get('second_currency'), \Illuminate\Support\Facades\Request::get('interval'));
 		}
 	});
 	Route::any('/trades', function(Exchange $exchange){
 		switch (Config::get('exchange.selected')) {
 			case 'kraken':
-				return $exchange->getTradeHistory(\Illuminate\Support\Facades\Request::get('pair'), \Illuminate\Support\Facades\Request::get('since'));
+				return $exchange->getTradeHistory(\Illuminate\Support\Facades\Request::get('first_currency'), \Illuminate\Support\Facades\Request::get('second_currency'), \Illuminate\Support\Facades\Request::get('since'));
 			case 'poloniex':
-				return $exchange->getTradeHistory(\Illuminate\Support\Facades\Request::get('pair'), \Illuminate\Support\Facades\Request::get('start'), \Illuminate\Support\Facades\Request::get('end'));
+				return $exchange->getTradeHistory(\Illuminate\Support\Facades\Request::get('first_currency'), \Illuminate\Support\Facades\Request::get('second_currency'), \Illuminate\Support\Facades\Request::get('start'), \Illuminate\Support\Facades\Request::get('end'));
 			default:
-				return $exchange->getTradeHistory(\Illuminate\Support\Facades\Request::get('pair'), \Illuminate\Support\Facades\Request::get('interval'));
+				return $exchange->getTradeHistory(\Illuminate\Support\Facades\Request::get('first_currency'), \Illuminate\Support\Facades\Request::get('second_currency'), \Illuminate\Support\Facades\Request::get('interval'));
 		}
 	});
 });
